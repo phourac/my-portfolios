d0d7990b830dfba26a0bc901df92716768fabe8a
Phourac <tphourac@gmail.com>
2024-09-12 16:49:59 +0700

    add metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,6 +4,16 @@ import FormHeader from "@/components/ContactPage/FormHeader";
 import Footer from "@/components/Footer";
 import { cookies } from "next/headers";
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  openGraph: {
+    title: "Contact - Than phourac",
+    description: "Your front-end developer",
+  },
+  title: "Contact",
+  description: "Your front-end developer",
+};
 
 function page() {
   return (
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -2,7 +2,15 @@ import Magnetic from "@/components/common/Magnetic";
 import Footer from "@/components/Footer";
 import WorkHead from "@/components/WorkPage/WorkHead";
 import React from "react";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  openGraph: {
+    title: "Work - Than phourac",
+    description: "Your front-end developer",
+  },
+  title: "Work",
+  description: "Your front-end developer",
+};
 const Work = () => {
   return (
     <>
